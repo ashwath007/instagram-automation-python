9abd18cc2e6a19015476f914680637de1a0e9039
vicky <srivicky2000@gmail.com>
2023-01-29 01:12:07 +0530

    working on: auto post image

--- insta-auto.py
+++ insta-auto.py
@@ -1,4 +1,6 @@
 
+import ait
+import pyautogui
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
@@ -16,6 +18,7 @@ import time
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.get("https://www.instagram.com/")
+driver.maximize_window
 inp_xpath_search = "/html/body/div[2]/div/div/div/div[1]/div/div/div/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[1]/div/label/input"
 input_box_search = WebDriverWait(driver,50).until(lambda driver: driver.find_element_by_xpath(inp_xpath_search))
 input_box_search.click()
@@ -57,18 +60,40 @@ time.sleep(2)
 # inp_xpath_new_btn_upload_btn.click()
 # time.sleep(2)
 
-ActionChains(driver).move_to_element( driver.find_element_by_xpath("/html/body/div[2]/div/div/div/div[2]/div/div/div[1]/div/div[3]/div/div/div/div/div[2]/div/div/div/div[2]/div[1]/div/div")).click().perform()
-handle = "[CLASS:#32770; TITLE:Open]"
-autoit.win_wait(handle, 60)
-autoit.control_set_text(handle, "Edit1", "F:\python-whatsapp-automation\pic.jpeg")
-autoit.control_click(handle, "Button1")
 
-
-inp_xpath_new_btn_upload_file = "/html/body/div[2]/div/div/div/div[2]/div/div/div[1]/div/div[3]/div/div/div/div/div[2]/div/div/div/div[2]/div[1]/div/div"
-inp_xpath_new_btn_upload_file = WebDriverWait(driver,50).until(lambda driver: driver.find_element_by_xpath(inp_xpath_new_btn_upload_file))
-inp_xpath_new_btn_upload_file.click()
-inp_xpath_new_btn_upload_file.send_keys('F:\python-whatsapp-automation\pic.jpeg')
 time.sleep(2)
+img_upload=driver.find_element_by_xpath('/html/body/div[2]/div/div/div/div[2]/div/div/div[1]/div/div[3]/div/div/div/div/div[2]/div/div/div/div[2]/div[1]/div/div/div[2]/div/button').click()
+time.sleep(2)
+path = "F:\python-whatsapp-automation\pic.jpeg" # your imagepath
+pyautogui.write(path) 
+time.sleep(2)
+pyautogui.press('enter')
+
+time.sleep(1)
+inp_xpat_image_next_btn = "/html/body/div[2]/div/div/div/div[2]/div/div/div[1]/div/div[3]/div/div/div/div/div[2]/div/div/div/div[1]/div/div/div[3]/div/button"
+inp_xpat_image_next_btn = WebDriverWait(driver,50).until(lambda driver: driver.find_element_by_xpath(inp_xpat_image_next_btn))
+inp_xpat_image_next_btn.click()
+time.sleep(1)
+
+inp_xpat_image_next_btn_one = "/html/body/div[2]/div/div/div/div[2]/div/div/div[1]/div/div[3]/div/div/div/div/div[2]/div/div/div/div[1]/div/div/div[3]/div/button"
+inp_xpat_image_next_btn_one = WebDriverWait(driver,50).until(lambda driver: driver.find_element_by_xpath(inp_xpat_image_next_btn_one))
+inp_xpat_image_next_btn_one.click()
+time.sleep(2)
+
+inp_xpat_caption_text = "/html/body/div[2]/div/div/div/div[2]/div/div/div[1]/div/div[3]/div/div/div/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div[2]/div[1]/textarea"
+inp_xpat_caption_text = WebDriverWait(driver,50).until(lambda driver: driver.find_element_by_xpath(inp_xpat_caption_text))
+inp_xpat_caption_text.send_keys("This post is posted by theDot Clan Bot")
+time.sleep(0.5)
+
+
+time.sleep(0.5)
+inp_xpat_share_btn = "/html/body/div[2]/div/div/div/div[2]/div/div/div[1]/div/div[3]/div/div/div/div/div[2]/div/div/div/div[1]/div/div/div[3]/div/button"
+inp_xpat_share_btn = WebDriverWait(driver,50).until(lambda driver: driver.find_element_by_xpath(inp_xpat_share_btn))
+inp_xpat_share_btn.click()
+time.sleep(0.5)
+
+# Post Image Upload Done
+
 
 
 
